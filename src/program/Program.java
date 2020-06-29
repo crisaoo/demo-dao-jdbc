7d603ec8a7d0c00f201cf2cfe5d33abe0edca39d
Crisaoo <Cristianocosta2019@outlook.com>
2020-06-29 13:56:59 -0300

    Seller entity class

--- src/program/Program.java
+++ src/program/Program.java
@@ -1,13 +1,19 @@
 package program;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
 		Department obj = new Department(5,"Books");
-		System.out.println(obj);
-	}
+		
+		Seller seller = new Seller(5, "Cristiano", "CristianoCosta@hotmail.com", new Date(), 1500.0 , obj);
 
+		System.out.println(seller);
+	}
 }
